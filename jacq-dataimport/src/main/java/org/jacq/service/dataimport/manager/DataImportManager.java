4f9125e5f75d15f07e1b4d73798f4ee4744c6fdd
viras <viras@users.sourceforge.net>
2016-03-05 09:18:37 +0100

    parsing and start of import routine

--- jacq-dataimport/src/main/java/org/jacq/service/dataimport/manager/DataImportManager.java
+++ jacq-dataimport/src/main/java/org/jacq/service/dataimport/manager/DataImportManager.java
@@ -18,13 +18,32 @@ package org.jacq.service.dataimport.manager;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.Charset;
-import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.StringUtils;
 import org.jacq.common.model.dataimport.ImportFile;
 import org.jacq.common.model.dataimport.ImportRecord;
+import org.jacq.common.model.jpa.TblAcquisitionDate;
+import org.jacq.common.model.jpa.TblAcquisitionEvent;
+import org.jacq.common.model.jpa.TblAcquisitionType;
+import org.jacq.common.model.jpa.TblAlternativeAccessionNumber;
+import org.jacq.common.model.jpa.TblBotanicalObject;
+import org.jacq.common.model.jpa.TblLivingPlant;
+import org.jacq.common.model.jpa.TblLocationCoordinates;
+import org.jacq.common.model.jpa.TblOrganisation;
 
 /**
  *
@@ -33,12 +52,19 @@ import org.jacq.common.model.dataimport.ImportRecord;
 @ManagedBean
 public class DataImportManager {
 
+    private static final Logger LOGGER = Logger.getLogger(DataImportManager.class.getName());
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    protected SimpleDateFormat separationDateFormat = new SimpleDateFormat("YYYY-mm-dd");
+
     /**
      * @throws java.io.IOException
      * @see
      * DataImportService#dataImport(org.jacq.common.model.dataimport.ImportFile)
      */
-    public void dataImport(ImportFile importFile) throws IOException {
+    public void dataImport(ImportFile importFile) throws IOException, ParseException {
         // Decode Base64 file content
         importFile.setFileContent(new String(Base64.getDecoder().decode(importFile.getFileContent()), Charset.forName("utf8")));
 
@@ -61,9 +87,77 @@ public class DataImportManager {
             importRecord.setLivingPlantNumber(record.get(i++));
             importRecord.setOriginalId(Long.valueOf(record.get(i++)));
             importRecord.setGatheringNumber(record.get(i++));
-            importRecord.setSeparationDate(Date.valueOf(record.get(i++)));
+            importRecord.setSeparationDate(separationDateFormat.parse(record.get(i++)));
             importRecord.setSeparationType(record.get(i++));
             importRecord.setLabelAnnotation(record.get(i++));
         }
+    }
+
+    /**
+     * Processes a record entry and writes the data to the database
+     *
+     * @param importRecord
+     */
+    @Transactional(rollbackOn = {Exception.class})
+    public void importRecord(ImportRecord importRecord) {
+        // check if we need to import a living plant
+        if (!StringUtils.isEmpty(importRecord.getLivingPlantNumber())) {
+            // check if entry already exists
+            TypedQuery<TblAlternativeAccessionNumber> alternativeAccessionNumberQuery = em.createNamedQuery("TblAlternativeAccessionNumber.findByNumber", TblAlternativeAccessionNumber.class);
+            alternativeAccessionNumberQuery.setParameter("number", importRecord.getLivingPlantNumber());
+            TblAlternativeAccessionNumber alternativeAccessionNumber = alternativeAccessionNumberQuery.getSingleResult();
+            if (alternativeAccessionNumber != null) {
+                LOGGER.log(Level.INFO, "Living-Plant Entry already exists: '{0}'", importRecord.getLivingPlantNumber());
+                return;
+            }
+
+            // no entry exists yet, start creating the data
+            // lookup default acqusition type
+            TypedQuery<TblAcquisitionType> acquisitionTypeQuery = em.createNamedQuery("TblAcquisitionType.findById", TblAcquisitionType.class);
+            acquisitionTypeQuery.setParameter("id", 1);
+            TblAcquisitionType acquisitionType = acquisitionTypeQuery.getSingleResult();
+            if (acquisitionType == null) {
+                throw new IllegalArgumentException("Unable to find acquisition type");
+            }
+
+            // start with the gathering location coordinates
+            TblLocationCoordinates locationCoordinates = new TblLocationCoordinates();
+            em.persist(locationCoordinates);
+
+            // setup gathering date
+            TblAcquisitionDate acquisitionDate = new TblAcquisitionDate();
+            em.persist(acquisitionDate);
+
+            // setup the gathering event
+            TblAcquisitionEvent acquisitionEvent = new TblAcquisitionEvent();
+            acquisitionEvent.setLocationCoordinatesId(locationCoordinates);
+            acquisitionEvent.setAcquisitionDateId(acquisitionDate);
+            acquisitionEvent.setAcquisitionTypeId(acquisitionType);
+            acquisitionEvent.setNumber(importRecord.getGatheringNumber());
+            em.persist(acquisitionEvent);
+
+            // lookup the organization by name
+            TypedQuery<TblOrganisation> organisationQuery = em.createNamedQuery("TblOrganisation.findById", TblOrganisation.class);
+            organisationQuery.setParameter("id", importRecord.getOrganizationId());
+            TblOrganisation organisation = organisationQuery.getSingleResult();
+            if (organisation == null) {
+                throw new IllegalArgumentException("Unable to find organisation");
+            }
+
+            // setup the botanical object
+            TblBotanicalObject botanicalObject = new TblBotanicalObject();
+            botanicalObject.setAcquisitionEventId(acquisitionEvent);
+            botanicalObject.setRecordingDate(new Date());
+            botanicalObject.setAnnotation(importRecord.getGenericAnnotation());
+            botanicalObject.setScientificNameId(1); // TODO query taxamatch
+            botanicalObject.setOrganisationId(organisation);
+            em.persist(botanicalObject);
+
+            // setup living plant object
+            TblLivingPlant livingPlant = new TblLivingPlant();
+            livingPlant.setId(botanicalObject.getId());
+            livingPlant.setLabelAnnotation(importRecord.getLabelAnnotation());
+        }
+
     }
 }
