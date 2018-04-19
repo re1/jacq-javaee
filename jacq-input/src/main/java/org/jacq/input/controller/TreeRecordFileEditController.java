655a0a3680ae4ac277ba9b8f9bb3e447e0f12114
Florian Hafner <f.hafner@senegate.at>
2018-04-19 13:48:43 +0200

    JACQ-396: FileUpload: fix imports of FileEditController

--- jacq-input/src/main/java/org/jacq/input/controller/TreeRecordFileEditController.java
+++ jacq-input/src/main/java/org/jacq/input/controller/TreeRecordFileEditController.java
@@ -20,15 +20,12 @@ import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import org.jacq.common.model.rest.TreeRecordFileResult;
 import org.jacq.common.rest.TreeRecordFileService;
 import org.jacq.common.util.ServicesUtil;
-import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 /**
