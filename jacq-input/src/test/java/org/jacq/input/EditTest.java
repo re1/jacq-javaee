9faf458e7ee6ed348500438546b8b7e4d006320d
Viras- <viras@users.sourceforge.net>
2018-06-11 11:10:17 +0200

    Adding basic testing setup

---
+++ jacq-input/src/test/java/org/jacq/input/EditTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 wkoller.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jacq.input;
+
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ *
+ * @author wkoller
+ */
+public class EditTest extends BaseTest {
+
+    @Test
+    public void openLiving() {
+        doLogin();
+
+        WebElement derivativeTable = driver.findElement(By.id("jacq_form:derivativeTable_data"));
+        Assert.assertNotNull(derivativeTable);
+        List<WebElement> derivativeRows = derivativeTable.findElements(By.tagName("tr"));
+        Assert.assertTrue(derivativeRows.size() > 0);
+
+        WebElement derivativeRow = derivativeRows.get(0);
+        Assert.assertNotNull(derivativeRow);
+        List<WebElement> derivativeButtons = derivativeRow.findElements(By.tagName("button"));
+        Assert.assertTrue(derivativeButtons.size() == 1);
+
+        WebElement derivativeEditButton = derivativeButtons.get(0);
+        Assert.assertNotNull(derivativeEditButton);
+        derivativeEditButton.click();
+
+        WebElement livingplantForm = driver.findElement(By.id("jacq_form:livingplant_form"));
+        Assert.assertNotNull(livingplantForm);
+    }
+
+}
